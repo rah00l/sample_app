ca595d9da9030fbc23514676906f3b062bb30200
Rahul Patil <rahulpatil2387@gmail.com>
2015-07-05 02:32:50 +0530

    Added associations of tags in movie and virtual attributes related to tag

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+
+  has_many :tags, :through => :taggings
+  has_many :taggings, :as => :taggable
+
+  def tag_name
+    tags.map(&:name).join(", ")
+  end
+
+  def tag_name=(name)
+    self.tags.clear
+    self.tags << Tag.find_or_create_by(name: name) unless name.blank?
+  end
 end
